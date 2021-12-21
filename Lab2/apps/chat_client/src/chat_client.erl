1c4c1798fa821e5c071b5fd34736ac9f1a48769b
Sarunas Buteikis <sarunas.kazimieras.buteikis@gmail.com>
2021-12-21 16:22:35 +0200

    Slightly refactored duplicate code

--- Lab2/apps/chat_client/src/chat_client.erl
+++ Lab2/apps/chat_client/src/chat_client.erl
@@ -43,9 +43,12 @@ terminate(_Reason, _State) ->
 %% Gen server calls
 %% ===================================================================
 
-handle_call({auth_client}, _From, State = #state{clientInfo={ServerNodeName,Username,Password}, authUsers = _, messageHistory = _}) ->
-  lager:info("ServerNodeName: ~p; Username: ~p; Password: ~p", [ServerNodeName, Username, Password]),
-
+handle_call(
+  {auth_client},
+  _From,
+  State = #state{clientInfo = {ServerNodeName, Username, Password}, authUsers = _, messageHistory = _}
+) ->
+  lager:info("ServerNodeName: ~p; Username: ~p; Password: ~p", [ServerNodeName, Username, Password]), 
   {?SERVER, ServerNodeName} ! {auth_client, Username, Password, node()},
   {reply, ok, State};
 
@@ -55,13 +58,18 @@ handle_call({get_state}, _From, State) ->
 %% Gen server casts
 %% ===================================================================
 
-
-handle_cast({get_authorized_clients}, State = #state{clientInfo={ServerNodeName,Username,Password}, authUsers = _, messageHistory = _}) ->
+handle_cast(
+  {get_authorized_clients},
+  State = #state{clientInfo = {ServerNodeName, Username, Password}, authUsers = _, messageHistory = _}
+) ->
   {?SERVER, ServerNodeName} ! {get_authorized_clients, Username, Password},
   {noreply, State};
-% Kai pirmas node'as siuncia broadcast'a kitiems
-handle_cast({broadcast, Message}, _State = #state{clientInfo = ClientInfo = {_,Username,_}, authUsers = Users, messageHistory = MessageHistory}) ->
-  io:format("~nKai node'as siuncia broadcast'a kitiems:~n"),
+% Node broadcasts message to other nodes. This is the original node that sends the broadcast
+% Called by the client API 'broadcast(Message)'
+handle_cast(
+  {broadcast, Message},
+  _State = #state{clientInfo = ClientInfo = {_, Username, _}, authUsers = Users, messageHistory = MessageHistory}
+) ->
   MessageTime = erlang:system_time(),
   lists:foldr(
     fun(Entry, Acc) ->
@@ -75,79 +83,74 @@ handle_cast({broadcast, Message}, _State = #state{clientInfo = ClientInfo = {_,U
     0,
     Users
   ),
-  % Create new state, won't use lists:append since it's quadratic complexity
-  NewState = #state{clientInfo = ClientInfo, authUsers = Users, messageHistory = [{Username, Message, MessageTime} | MessageHistory]},
+  NewState = #state{
+    clientInfo = ClientInfo,
+    authUsers = Users,
+    messageHistory = [{Username, Message, MessageTime} | MessageHistory]
+  },
   {noreply, NewState};
-% Kai node'as persiuncia zinute kitiems, bet cia pirma nauja zinute
-handle_cast({broadcast, SenderUsername, Message, SenderTimestamp}, _State = #state{clientInfo = {_,Username,_}, authUsers = Users, messageHistory = []}) ->
-  io:format("~nKai node'as persiuncia zinute kitiems, bet cia pirma nauja zinute paciam node'ui:~n"),
-  lists:foldr(
-    fun(Entry, Acc) ->
-      {RecipientUserName, RecipientNode} = Entry,
-      case Username of
-        RecipientUserName -> Acc; % Do nothing
-        _ ->
-          {?CLIENT, RecipientNode} ! {receive_message, SenderUsername, Message, SenderTimestamp},
-          Acc % Do nothing with the accumulator.
-      end
-    end,
-    0,
-    Users
-  ),
-  % Create new state, won't use lists:append since it's quadratic complexity
-  % NewState = #state{clientInfo = ClientInfo, authUsers = Users, messageHistory = [{Username, Message, SenderTimestamp}]},
+% Node re-sends message to other nodes when the message history is empty
+% This broadcast is called by handle_info, so no need to update state
+handle_cast(
+  {broadcast, SenderUsername, Message, SenderTimestamp},
+  _State = #state{clientInfo = {_,Username,_}, authUsers = Users, messageHistory = []}
+) ->
+
+  send_message_all_but_current(Username, Users, SenderUsername, Message, SenderTimestamp),
   {noreply, _State};
-% Kai node'as persiuncia zinute kitiems
-handle_cast({broadcast, SenderUsername, Message, SenderTimestamp}, _State = #state{clientInfo = {_,Username,_}, authUsers = Users, messageHistory = MessageHistory}) ->
-  io:format("~nKai node'as persiuncia zinute:~n"),
-  lists:foldr(
-    fun(Entry, Acc) ->
-      {RecipientUserName, RecipientNode} = Entry,
-      case Username of
-        RecipientUserName -> Acc; % Do nothing
-        _ ->
-          case lists:member({SenderUsername, Message, SenderTimestamp}, MessageHistory) of
-            true -> Acc;
-            false -> 
-              {?CLIENT, RecipientNode} ! {receive_message, SenderUsername, Message, SenderTimestamp},
-              Acc % Do nothing with the accumulator.
-          end
-      end
+% Node sends received message to others when there is existing message history
+% No need to update state, since it's updated in handle_info, which this method is called from
+handle_cast(
+  {broadcast, SenderUsername, Message, SenderTimestamp},
+  _State = #state{clientInfo = {_,Username,_}, authUsers = Users, messageHistory = MessageHistory}
+) ->
+  
+  case lists:member({SenderUsername, Message, SenderTimestamp}, MessageHistory) of
+    true -> true;
+    false -> 
+      send_message_all_but_current(Username, Users, SenderUsername, Message, SenderTimestamp)
     end,
-    0,
-    Users
-  ),
-  % Create new state, won't use lists:append since it's quadratic complexity
-  % NewState = #state{clientInfo = ClientInfo, authUsers = Users, messageHistory = [{Username, Message, SenderTimestamp} | MessageHistory]},
   {noreply, _State}.
 
 %% ===================================================================
 %% Gen server infos
 %% ===================================================================
 
-handle_info({get_authorized_clients_from_server, Users}, _State = #state{clientInfo = ClientInfo, authUsers = _, messageHistory = MessageHistory}) ->
+handle_info(
+  {get_authorized_clients_from_server, Users},
+  _State = #state{clientInfo = ClientInfo, authUsers = _, messageHistory = MessageHistory}
+) ->
   NewState = #state{clientInfo = ClientInfo, authUsers = Users, messageHistory = MessageHistory},
   lager:info("Got all authorized clients, here they are:~p~n", [NewState]),
   {noreply, NewState};
 
-handle_info({receive_message, SenderUsername, Message, SenderTimestamp}, State = #state{clientInfo = ClientInfo, authUsers = AuthUsers, messageHistory = []}) ->
-  io:format("~nKai node'as gauna zinute, neturedamas zinuciu istorijos:~n"),
-  StateWithoutSender = #state{clientInfo = ClientInfo, authUsers = [ T || {K, _} = T <-  AuthUsers, K =/= SenderUsername], messageHistory = []},
+handle_info(
+  {receive_message, SenderUsername, Message, SenderTimestamp},
+  State = #state{clientInfo = ClientInfo, authUsers = AuthUsers, messageHistory = []}
+) ->
+  StateWithoutSender = #state{
+    clientInfo = ClientInfo,
+    authUsers = sub_list_without_tuple_by_key(AuthUsers, SenderUsername),
+    messageHistory = []
+  },
   handle_cast({broadcast, SenderUsername, Message, SenderTimestamp}, StateWithoutSender),
   
   NewState = State#state{messageHistory = [{SenderUsername, Message, SenderTimestamp}]},
   {noreply, NewState};
 
-handle_info({receive_message, SenderUsername, Message, SenderTimestamp}, State = #state{clientInfo = ClientInfo, authUsers = AuthUsers, messageHistory = MessageHistory}) ->
-  io:format("~nKai node'as gauna zinute, turedamas zinuciu istorija:~n"),
-  {CurrentLatestUsername, CurrentLatestMessage, CurrentLatestTimestamp} = lists:nth(1, MessageHistory),
-  io:format("~nStai yra paskutinis username'as ir gavejo username'as: ~p ir ~p, ~npaskutine zinute ir gavejo zinute: ~p ir ~p ~npaskutinis timestamp'as ir gavejo timestamp'as: ~p ir ~p~n", [CurrentLatestUsername, SenderUsername, CurrentLatestMessage, Message, CurrentLatestTimestamp, SenderTimestamp]),
-  io:format("~nAr jau yra egzistuojanti zinutes istorija?~p~n", [lists:member({SenderUsername, Message, SenderTimestamp}, MessageHistory)]),
-  io:format("~nVISA ZINUCIU ISTORIJA~p~n", [MessageHistory]),
+handle_info(
+  {receive_message, SenderUsername, Message, SenderTimestamp},
+  State = #state{clientInfo = ClientInfo, authUsers = AuthUsers, messageHistory = MessageHistory}
+) ->
   NewState = case lists:member({SenderUsername, Message, SenderTimestamp}, MessageHistory) of
     true -> State;
     false -> 
-      StateWithoutSender = #state{clientInfo = ClientInfo, authUsers = [ T || {K, _} = T <-  AuthUsers, K =/= SenderUsername], messageHistory = MessageHistory},
+      StateWithoutSender = 
+        #state{
+          clientInfo = ClientInfo,
+          authUsers = sub_list_without_tuple_by_key(AuthUsers, SenderUsername),
+          messageHistory = MessageHistory
+        },
       handle_cast({broadcast, SenderUsername, Message, SenderTimestamp}, StateWithoutSender),
       State#state{messageHistory = [{SenderUsername, Message, SenderTimestamp} | MessageHistory]}
     end,
@@ -176,9 +179,26 @@ get_authorized_clients() ->
 broadcast(Message) ->
   ok = gen_server:cast(?CLIENT, {broadcast, Message}),
   {noreply, ok}.
-% IMPLEMENT URB?
-% Iterate through users and message pass to each user
-% Kaip patikrinti ar first reception'as?
-% Patikrinus ar pirma karta gauta zinute m, tada kiekvienam procesui,
-% Iskyrus einamajam procesui ir procesui, kuris siunte zinute, issiusti visiems kitiems
-% State visi node'ai, i map'o state'a pasirasyt kad buvo gauta ta zinute
+
+%% ===================================================================
+%% Internal Methods
+%% ===================================================================
+% iterate and send message to users except for current node based on authenticated user list
+send_message_all_but_current(ClientUsername, AuthUserList, SenderUsername, Message, SenderTimestamp) ->
+  lists:foldr(
+    fun(Entry, Acc) ->
+      {RecipientUserName, RecipientNode} = Entry,
+      case ClientUsername of
+        RecipientUserName -> Acc; % Do nothing
+        _ ->
+              {?CLIENT, RecipientNode} ! {receive_message, SenderUsername, Message, SenderTimestamp},
+              Acc % Do nothing with the accumulator.
+      end
+  end,
+  0,
+  AuthUserList
+  ).
+% create sublist without a specific user
+sub_list_without_tuple_by_key(List, ValueOfKey) ->
+  [ T || {K, _} = T <-  List, K =/= ValueOfKey].
+
